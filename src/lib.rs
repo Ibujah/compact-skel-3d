1c76d6021c64fbe381592ca9984d05f25bea98c6
Bastien <dbastien71@gmail.com>
2023-02-24 09:14:11 +0100

    Started skeleton structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub mod mesh3d;
+pub mod skeleton3d;
 pub mod algorithm;
---
+++ src/skeleton3d/skeleton3d.rs
@@ -0,0 +1,9 @@
+use anyhow::Result;
+use nalgebra::base::*;
+
+pub type Node = [usize; 4];
+pub type Edge = [usize; 3];
+pub type Alveola = [usize; 2];
+
+pub struct Skeleton3D {
+}
